b0d2519277a3eb1117a4e32534c4eed1a1daa4de
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-23 14:52:12 -0400

    yeet

--- csvmaker.py
+++ csvmaker.py
@@ -2,6 +2,6 @@ hashesFileOperation = open("malwareHashes.txt", "r")
 hashes = hashesFileOperation.read().split('\n')
 hashesFileOperation = open("malwareHashes.csv", "a")
 for x in range(len(hashes)):
-    hashesFileOperation.write(hashes[x]+",\n")
+    hashesFileOperation.write(hashes[x]+",1\n")
 
 #print(hashes[0])
