3d4d6758b5a9252504fa78c42f9b2405819c5292
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-23 13:40:26 -0400

    desktopApp

--- desktopApp.py
+++ desktopApp.py
@@ -1,23 +1,13 @@
 from tkinter import Tk, Frame, Button, END
 
 class Application(Frame):
-    def say_hi(self):
-        print("hi there, everyone!")
-
     def createWidgets(self):
         self.winfo_toplevel().title("Chungus")
         self.QUIT = Button(self)
         self.QUIT["text"] = "QUIT"
         self.QUIT["fg"]   = "red"
         self.QUIT["command"] =  self.quit
-
         self.QUIT.pack({"side": "left"})
-
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
-
-        self.hi_there.pack({"side": "left"})
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -25,7 +15,6 @@ class Application(Frame):
         self.createWidgets()
 
 root = Tk()
-
 app = Application(master=root)
 app.mainloop()
 root.destroy()
