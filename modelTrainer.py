488b1f3e94b276da328f5c6391978d9cec7f7aa0
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-23 16:48:21 -0400

    new model new aids

--- modelTrainer.py
+++ modelTrainer.py
@@ -10,11 +10,13 @@ import numpy
 from numpy import array
 # fix random seed for reproducibility
 numpy.random.seed(7)
-dataset = numpy.loadtxt("malwareHashes.csv")
-
+dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:1]
 Y = dataset[:,1]
+print(X)
+print("---------")
+print(Y)
 
 model = Sequential()
 model.add(Dense(12, input_dim=1, activation='relu'))
@@ -22,12 +24,6 @@ model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, epochs=300, batch_size=15, verbose=0)
-
+model.fit(X, Y, epochs=300, batch_size=15)
 scores = model.evaluate(X, Y)
-
-import sys
-def main(gsr):
-        print(model.predict(array([gsr]))[0][0])
-if __name__ == '__main__':
-        main(float(sys.argv[1]))
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
--- stolencsvparser.py
+++ stolencsvparser.py
@@ -1,12 +1,17 @@
-import numpy
-from numpy import array
-
 hashesFile = open('stolen.txt', 'r')
 hashesFile = hashesFile.read()
 hashes = hashesFile.split('\n')
+def getOrds(hash):
+    y = list(hash)
+    s = ''
+    for x in range(len(y)):
+        s+=str(ord(y[x]))
+    return s
 
 hashesFileOperation = open("dataset.csv", "a")
 for x in range(len(hashes)):
     hashes[x] = hashes[x].split('|')
     a = list(hashes[x])
-    hashesFileOperation.write(a[0]+','+a[1]+','+a[-1]+'\n')
+    a[1] = getOrds(a[1])
+    hashesFileOperation.write(a[1]+','+a[-1]+'\n')
+
