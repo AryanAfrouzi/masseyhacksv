67075ad4d994d22fced589dddd97fad704c54ab2
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-24 07:00:52 -0400

    pefile code

--- modelTrainer.py
+++ modelTrainer.py
@@ -12,7 +12,7 @@ from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
 import math
-
+import pefile
 import numpy
 from numpy import array
 # fix random seed for reproducibility
