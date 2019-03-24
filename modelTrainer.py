3468751f8d7f12b1ce12a0782afd4f77f9867bb7
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-24 01:15:18 -0400

    sadlads4returnofthesadlads

--- modelTrainer.py
+++ modelTrainer.py
@@ -6,8 +6,10 @@ cgitb.enable()
 
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam
+from keras.optimizers import Adadelta
 from keras.utils import to_categorical
+
+from sklearn.model_selection import train_test_split
 
 import math
 
@@ -19,6 +21,7 @@ dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:54]
 Y = to_categorical(dataset[:,54])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33)
 #print(X)
 #print("---------")
 #print(Y)
@@ -48,15 +51,11 @@ model = Sequential()
 model.add(Dense(55, input_dim=54, activation='relu'))
 model.add(Dense(36, activation='relu'))
 model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
 model.add(Dense(2, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0001), metrics=['accuracy'])
-model.fit(X, Y, epochs=20, batch_size=32, shuffle=True)
-
-test_dataset = numpy.loadtxt("testdata.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X_test = test_dataset[:,0:54]
-Y_test = to_categorical(test_dataset[:,54])
+model.compile(loss='binary_crossentropy', optimizer=Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0), metrics=['accuracy'])
+model.fit(X_train, Y_train, epochs=10, batch_size=100, shuffle=True)
 
 scores = model.evaluate(X_test, Y_test)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
