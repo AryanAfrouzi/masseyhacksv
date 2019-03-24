fe9ce2ae3eda756d132c012528682cf96ae446c7
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-23 21:15:39 -0400

    onehot and normalization

--- modelTrainer.py
+++ modelTrainer.py
@@ -6,6 +6,9 @@ cgitb.enable()
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+
 import numpy
 from numpy import array
 # fix random seed for reproducibility
@@ -13,18 +16,34 @@ numpy.random.seed(7)
 dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:54]
-Y = dataset[:,54]
-print(X)
-print("---------")
-print(Y)
+Y = to_categorical(dataset[:,54])
+#print(X)
+#print("---------")
+#print(Y)
+
+#data normalization
+
+def normalize(column_id):
+    column = []
+    for x in range(len(X[0])):
+        column.append(X[x][column_id])
+    cmax = max(column)
+    cmin = min(column)
+    for x in range(len(X[0])):
+        cval = X[x][column_id]
+        X[x][column_id] = (cval - cmin) / (cmax - cmin)
+    return column
+    #normalized_data = (data - minimum) / (maximum - minimum)
+print(normalize(0))
 
 model = Sequential()
-model.add(Dense(12, input_dim=54, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(55, input_dim=54, activation='relu'))
+model.add(Dense(36, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(2, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, epochs=200, batch_size=20)
+model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.05), metrics=['accuracy'])
+model.fit(X, Y, epochs=20, batch_size=10, shuffle=True)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
